440df2d4b57884059012c4b8394d1eaeff569d2f
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-02-19 10:32:19 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/shogunpurple/retreat/master/retreat?token=ACV4GVYROVXSXCY3DGWRIBC6JUDGQ -o /usr/local/bin/retreat
+curl https://raw.githubusercontent.com/shogunpurple/retreat/master/retreat -o /usr/local/bin/retreat
 
 chmod +rx /usr/local/bin/retreat
 
